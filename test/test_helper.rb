dd9153eedc0cab8a174f4f6646f6ba6a6a2c2a9e
Tim Gentry <52189+timgentry@users.noreply.github.com>
2023-04-06 19:43:55 +0100

    Added simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# SimpleCov must be required and started before everything else
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'active_support'
 require 'rails/generators/test_case'
