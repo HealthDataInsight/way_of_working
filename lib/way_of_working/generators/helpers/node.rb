e57f18fa9e7e23918e3c80094de0f9c9d0f71d3a
Tim Gentry <52189+timgentry@users.noreply.github.com>
2023-04-11 15:18:17 +0100

    Interim commit of Node helpers

---
+++ lib/way_of_working/generators/helpers/node.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+require 'rainbow'
+require 'thor'
+require 'way_of_working/generators/helpers/shell'
+require 'yaml'
+
+module WayOfWorking
+  module Generators
+    module Helpers
+      # Node helper methods for generators
+      module Node
+        extend ActiveSupport::Concern
+
+        include Thor::Actions
+        include WayOfWorking::Generators::Helpers::Shell
+
+        YARNRC_FILENAME = '.yarnrc.yml'
+        NVMRC_FILENAME = '.nvmrc'
+
+        private
+
+        def npm_package_manager_used?
+          file_exist?('package-lock.json') || file_exist?('npm-shrinkwrap.json')
+        end
+
+        def yarn_package_manager_used?
+          yarn_lock_file = 'yarn.lock'
+
+          # Check if there is a yarnrc file that specifies a different lock file name
+          if file_exist?(YARNRC_FILENAME)
+            yarnrc_hash = YAML.safe_load(File.read(File.join(destination_root, YARNRC_FILENAME)))
+            yarn_lock_file = yarnrc_hash.fetch('lockfileFilename', yarn_lock_file)
+          end
+
+          file_exist?(yarn_lock_file)
+        end
+
+        # This returns :npm or :yarn depending on the project config or user choice.
+        def node_package_manager
+          return :npm if npm_package_manager_used?
+          return :yarn if yarn_package_manager_used?
+
+          # Not obvious from the code, so ask
+          answer = ask_for_response('Which javascript package manager do you wish to use? ' \
+                                    '(yarn/npm)')
+          return :yarn if /\Ayarn/i.match?(answer)
+          return :npm if /\Anpm/i.match?(answer)
+
+          say_error('No recognisable answer given, finishing.', :red)
+          abort
+        end
+
+        def nvm_node_version
+          return unless file_exist?(NVMRC_FILENAME)
+
+          File.read(File.join(destination_root, NVMRC_FILENAME)).strip
+        end
+
+        def nvm_node_version_description
+          return unless nvm_node_version
+          return 'the latest Long Term Support (LTS) version of node' if nvm_node_version == 'lts/*'
+
+          "node version #{nvm_node_version}"
+        end
+
+        def install_nvm
+          return if installed?(:nvm)
+
+          answer = ask_for_response('Do you wish to install nvm via Homebrew? (y/n)')
+
+          if /\Ay/i.match?(answer)
+            # Install dependencies
+            install_homebrew
+
+            say 'Installing nvm...'
+            system('brew install nvm')
+          else
+            abort("\nVisit https://github.com/nvm-sh/nvm#installing-and-updating to " \
+                  'install nvm and re-run this command.')
+          end
+        end
+
+        def install_node
+          return if installed?(:node)
+
+          answer = ask_for_response('Do you wish to install node via nvm (y/n)')
+
+          if /\Ay/i.match?(answer)
+            # Install dependencies
+            install_nvm
+
+            create_file('.nvmrc', 'lts/*') unless file_exist?('.nvmrc')
+
+            say 'Installing node...'
+            system('. $NVM_DIR/nvm.sh && nvm use')
+          else
+            abort("\nVisit https://nodejs.org to install node")
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/generators/helpers/node_test.rb
@@ -0,0 +1,354 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'way_of_working/generators/helpers/node'
+
+module WayOfWorking
+  module Generators
+    module Helpers
+      # This is a Thor generator used to test the package manager helper methods
+      class NodePackageManagerTester < Thor::Group
+        include WayOfWorking::Generators::Helpers::Node
+
+        FINISHED = 'Finished successfully!'
+
+        def output_node_package_manager
+          say("node_package_manager: #{node_package_manager.inspect}")
+        end
+
+        def finished
+          say(FINISHED)
+        end
+      end
+
+      # This is a Thor generator used to test the version helper methods
+      class NodeVersionTester < Thor::Group
+        include WayOfWorking::Generators::Helpers::Node
+
+        FINISHED = 'Finished successfully!'
+
+        def output_nvm_node_version
+          say("nvm_node_version: #{nvm_node_version.inspect}")
+          say("nvm_node_version_description: #{nvm_node_version_description.inspect}")
+        end
+
+        def finished
+          say(FINISHED)
+        end
+      end
+
+      # This is a Thor generator used to test the nvm installer helper method
+      class NodeInstallNVMTester < Thor::Group
+        include WayOfWorking::Generators::Helpers::Node
+
+        FINISHED = 'Finished successfully!'
+
+        def now_install_nvm
+          install_nvm
+        end
+
+        def finished
+          say(FINISHED)
+        end
+      end
+
+      # This is a Thor generator used to test the node installer helper method
+      class NodeInstallNodeTester < Thor::Group
+        include WayOfWorking::Generators::Helpers::Node
+
+        FINISHED = 'Finished successfully!'
+
+        def now_install_node
+          install_node
+        end
+
+        def finished
+          say(FINISHED)
+        end
+      end
+
+      # This class tests the EndToEndTests::Init Thor Group (generator)
+      class NodeTest < Rails::Generators::TestCase
+        # tests NodePackageManagerTester
+        destination WayOfWorking.root.join('tmp/generators')
+        setup :prepare_destination
+
+        test 'generator runs without errors' do
+          self.generator_class = NodePackageManagerTester
+          copy WayOfWorking.root.join('test', 'resources', 'cypress_package-lock.json'),
+               destination_root.join('package-lock.json')
+
+          assert_nothing_raised do
+            run_generator
+          end
+        end
+
+        test 'node_package_manager with package-lock.json' do
+          self.generator_class = NodePackageManagerTester
+
+          copy WayOfWorking.root.join('test', 'resources', 'cypress_package-lock.json'),
+               destination_root.join('package-lock.json')
+
+          assert_file 'package-lock.json'
+
+          stdout = run_generator
+          assert_match('node_package_manager: :npm', stdout)
+        end
+
+        test 'node_package_manager with npm-shrinkwrap.json' do
+          self.generator_class = NodePackageManagerTester
+
+          copy WayOfWorking.root.join('test', 'resources', 'cypress_npm-shrinkwrap.json'),
+               destination_root.join('npm-shrinkwrap.json')
+
+          assert_file 'npm-shrinkwrap.json'
+
+          stdout = run_generator
+          assert_match('node_package_manager: :npm', stdout)
+        end
+
+        test 'node_package_manager with yarn.lock' do
+          self.generator_class = NodePackageManagerTester
+
+          copy WayOfWorking.root.join('test', 'resources', 'cypress_yarn.lock'),
+               destination_root.join('yarn.lock')
+
+          assert_file 'yarn.lock'
+
+          stdout = run_generator
+          assert_match('node_package_manager: :yarn', stdout)
+        end
+
+        test 'node_package_manager with .yarnrc.yml and custom yarn.lock' do
+          self.generator_class = NodePackageManagerTester
+
+          copy WayOfWorking.root.join('test', 'resources', 'cypress_yarn.lock'),
+               destination_root.join('custom_location_yarn.lock')
+          copy WayOfWorking.root.join('test', 'resources', 'custom_location_yarnrc.yml'),
+               destination_root.join('.yarnrc.yml')
+
+          assert_file '.yarnrc.yml'
+          assert_file 'custom_location_yarn.lock'
+
+          stdout = run_generator
+          assert_match('node_package_manager: :yarn', stdout)
+        end
+
+        test 'node_package_manager with no lock file and npm choice' do
+          self.generator_class = NodePackageManagerTester
+
+          generator_class.any_instance.expects(:ask).
+            with('Which javascript package manager do you wish to use? (yarn/npm)').
+            returns('NpM')
+
+          stdout = run_generator
+          assert_match('node_package_manager: :npm', stdout)
+        end
+
+        test 'node_package_manager with no lock file and yarn choice' do
+          self.generator_class = NodePackageManagerTester
+
+          generator_class.any_instance.expects(:ask).
+            with('Which javascript package manager do you wish to use? (yarn/npm)').
+            returns('yaRn')
+
+          stdout = run_generator
+          assert_match('node_package_manager: :yarn', stdout)
+        end
+
+        test 'node_package_manager with no lock file and invalid choice' do
+          self.generator_class = NodePackageManagerTester
+
+          generator_class.any_instance.expects(:ask).
+            with('Which javascript package manager do you wish to use? (yarn/npm)').
+            returns('bob')
+
+          stdout = ''
+          stderr = capture(:stderr) do
+            assert_raise ::SystemExit do
+              stdout = run_generator
+            end
+          end
+
+          assert_match('No recognisable answer given, finishing.', stderr)
+          assert_no_match('node_package_manager:', stdout)
+          assert_no_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'node_package_manager with no lock file and no choice' do
+          self.generator_class = NodePackageManagerTester
+          generator_class.any_instance.expects(:ask).
+            with('Which javascript package manager do you wish to use? (yarn/npm)').
+            returns('')
+
+          stdout = ''
+          stderr = capture(:stderr) do
+            assert_raise ::SystemExit do
+              stdout = run_generator
+            end
+          end
+
+          assert_match('No answer given, finishing.', stderr)
+          assert_no_match('node_package_manager:', stdout)
+          assert_no_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'nvm_node_version with node 18' do
+          self.generator_class = NodeVersionTester
+
+          File.write(destination_root.join(generator_class::NVMRC_FILENAME), '18')
+
+          assert_file generator_class::NVMRC_FILENAME
+
+          stdout = run_generator
+          assert_match('nvm_node_version: "18"', stdout)
+          assert_match('nvm_node_version_description: "node version 18"', stdout)
+        end
+
+        test 'nvm_node_version with LTS node' do
+          self.generator_class = NodeVersionTester
+
+          File.write(destination_root.join(generator_class::NVMRC_FILENAME), 'lts/*')
+
+          assert_file generator_class::NVMRC_FILENAME
+
+          stdout = run_generator
+          assert_match('nvm_node_version: "lts/*"', stdout)
+          assert_match('nvm_node_version_description: "the latest Long Term Support (LTS) version of node"', stdout)
+        end
+
+        test 'nvm_node_version with no nvmrc file' do
+          self.generator_class = NodeVersionTester
+
+          assert_no_file generator_class::NVMRC_FILENAME
+
+          stdout = run_generator
+          assert_match('nvm_node_version: nil', stdout)
+          assert_match('nvm_node_version_description: nil', stdout)
+        end
+
+        test 'install_nvm with nvm already installed' do
+          self.generator_class = NodeInstallNVMTester
+
+          generator_class.any_instance.expects(:installed?).with(:nvm).returns(true)
+          generator_class.any_instance.expects(:ask_for_response).never
+
+          stdout = run_generator
+          assert_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_nvm with user not wanting to install' do
+          self.generator_class = NodeInstallNVMTester
+
+          generator_class.any_instance.expects(:installed?).with(:nvm).returns(false)
+          generator_class.any_instance.expects(:ask).
+            with('Do you wish to install nvm via Homebrew? (y/n)').
+            returns('nO')
+          generator_class.any_instance.expects(:install_homebrew).never
+
+          stdout = ''
+          stderr = capture(:stderr) do
+            assert_raise ::SystemExit do
+              stdout = run_generator
+            end
+          end
+
+          assert_match('install nvm and re-run this command.', stderr)
+          assert_no_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_nvm with user wanting to install' do
+          self.generator_class = NodeInstallNVMTester
+
+          generator_class.any_instance.expects(:installed?).with(:nvm).returns(false)
+          generator_class.any_instance.expects(:ask).
+            with('Do you wish to install nvm via Homebrew? (y/n)').
+            returns('YeS')
+          generator_class.any_instance.expects(:install_homebrew).returns(nil)
+          ::Object.any_instance.stubs(:system).with('brew install nvm').returns(true)
+
+          stdout = run_generator
+
+          assert_match('Installing nvm...', stdout)
+          assert_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_node with node already installed' do
+          self.generator_class = NodeInstallNodeTester
+
+          generator_class.any_instance.expects(:installed?).with(:node).returns(true)
+          generator_class.any_instance.expects(:ask_for_response).never
+
+          stdout = run_generator
+          assert_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_node with user not wanting to install' do
+          self.generator_class = NodeInstallNodeTester
+
+          generator_class.any_instance.expects(:installed?).with(:node).returns(false)
+          generator_class.any_instance.expects(:ask).
+            with('Do you wish to install node via nvm (y/n)').
+            returns('nO')
+          generator_class.any_instance.expects(:install_nvm).never
+
+          stdout = ''
+          stderr = capture(:stderr) do
+            assert_raise ::SystemExit do
+              stdout = run_generator
+            end
+          end
+
+          assert_match('Visit https://nodejs.org to install node', stderr)
+          assert_no_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_node with user wanting to install without nvmrc file' do
+          self.generator_class = NodeInstallNodeTester
+
+          generator_class.any_instance.expects(:installed?).with(:node).returns(false)
+          generator_class.any_instance.expects(:ask).
+            with('Do you wish to install node via nvm (y/n)').
+            returns('YeS')
+          generator_class.any_instance.expects(:install_nvm).returns(nil)
+          ::Object.any_instance.stubs(:system).with('. $NVM_DIR/nvm.sh && nvm use').returns(true)
+
+          assert_no_file generator_class::NVMRC_FILENAME
+
+          stdout = run_generator
+
+          assert_file generator_class::NVMRC_FILENAME do |content|
+            assert_match('lts/*', content)
+          end
+          assert_match('Installing node...', stdout)
+          assert_match(generator_class::FINISHED, stdout)
+        end
+
+        test 'install_node with user wanting to install with nvmrc file' do
+          self.generator_class = NodeInstallNodeTester
+
+          generator_class.any_instance.expects(:installed?).with(:node).returns(false)
+          generator_class.any_instance.expects(:ask).
+            with('Do you wish to install node via nvm (y/n)').
+            returns('YeS')
+          generator_class.any_instance.expects(:install_nvm).returns(nil)
+          ::Object.any_instance.stubs(:system).with('. $NVM_DIR/nvm.sh && nvm use').returns(true)
+
+          File.write(destination_root.join(generator_class::NVMRC_FILENAME), '18')
+
+          assert_file generator_class::NVMRC_FILENAME do |content|
+            assert_match('18', content)
+          end
+
+          stdout = run_generator
+
+          assert_file generator_class::NVMRC_FILENAME do |content|
+            assert_match('18', content)
+          end
+          assert_match('Installing node...', stdout)
+          assert_match(generator_class::FINISHED, stdout)
+        end
+      end
+    end
+  end
+end
