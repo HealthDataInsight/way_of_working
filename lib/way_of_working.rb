6e5dfaface7acd0bb303e16126b5b10e36c527f4
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-15 08:58:58 +0100

    Added support for Way of Working plugins (#68)

--- lib/way_of_working.rb
+++ lib/way_of_working.rb
@@ -8,3 +8,13 @@ require_relative 'way_of_working/version'
 module WayOfWorking
   class Error < StandardError; end
 end
+
+# Automatically load way_of_working plugins.
+Gem::Specification.
+  select { |gemspec| gemspec.name =~ /\Away_of_working-/ }.
+  collect(&:name).uniq. # multiple versions of a single gem may be installed at once
+  each do |name|
+  require "#{name.gsub('-', '/')}/plugin"
+rescue LoadError
+  warn(Rainbow("Failed to load #{name} plugin.\n").red)
+end
