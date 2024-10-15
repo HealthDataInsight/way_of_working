4157bab800c6e87b6e57e3737d3122ad6a4961df
Tim Gentry <52189+timgentry@users.noreply.github.com>
2024-10-15 12:08:14 +0100

    Namespaced SubCommandBase

--- lib/way_of_working/sub_command_base.rb
+++ lib/way_of_working/sub_command_base.rb
@@ -2,15 +2,17 @@
 
 require 'thor'
 
-# This class enables subcommands to work correctly with help. See:
-# https://github.com/rails/thor/wiki/Subcommands#subcommands-that-work-correctly-with-help
-class SubCommandBase < Thor
-  def self.banner(command, _namespace = nil, _subcommand = false)
-    "#{basename} #{subcommand_prefix} #{command.usage}"
-  end
+module WayOfWorking
+  # This class enables subcommands to work correctly with help. See:
+  # https://github.com/rails/thor/wiki/Subcommands#subcommands-that-work-correctly-with-help
+  class SubCommandBase < Thor
+    def self.banner(command, _namespace = nil, _subcommand = false)
+      "#{basename} #{subcommand_prefix} #{command.usage}"
+    end
 
-  def self.subcommand_prefix
-    name.gsub(/.*::/, '').gsub(/^[A-Z]/) { |match| match[0].downcase }.
-      gsub(/[A-Z]/) { |match| "-#{match[0].downcase}" }
+    def self.subcommand_prefix
+      name.gsub(/.*::/, '').gsub(/^[A-Z]/) { |match| match[0].downcase }.
+        gsub(/[A-Z]/) { |match| "-#{match[0].downcase}" }
+    end
   end
 end
